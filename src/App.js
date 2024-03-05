ce20df993cee8db812d51e5acafd38162ec5d2eb
888Shubham <shubham.kb888@gmail.com>
2024-03-05 22:51:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import React from "react";
+import ExpenseForm from "./components/ExpenseForm/ExpenseForm";
+import ExpenseInfo from "./components/ExpenseInfo/ExpenseInfo";
+import ExpenseList from "./components/ExpenseList/ExpenseList";
+
+function App(){
+  
+    return (
+      <>
+        <h2 className="mainHeading">Expense Tracker</h2>
+        <div className="App">
+          <ExpenseForm/>
+          <div className="expenseContainer">
+            <ExpenseInfo/>
+            <ExpenseList/>
+          </div>
+        </div>
+      </>
+    );
+  
+}
+export default App;
+
+
+
---
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -0,0 +1,35 @@
+import React,{useRef} from "react";
+import styles from "./ExpenseForm.module.css";
+
+
+
+function ExpenseForm(){
+  const expenseTextInput = useRef();
+  const expenseAmountInput = useRef();
+  return (
+    <form className={styles.form} onSubmit={() => {}}>
+      <h3>Add new transaction</h3>
+      <label htmlFor="expenseText">Text</label>
+      <input
+        id="expenseText"
+        className={styles.input}
+        type="text"
+        placeholder="Enter text..."
+        required
+      />
+      <div>
+        <label htmlFor="expenseAmount">Amount</label>
+        <div>(negative - expense,positive-income)</div>
+      </div>
+      <input
+        className={styles.input}
+        id="expenseAmount"
+        type="number"
+        placeholder="Enter amount..."
+        required
+      />
+      <button className={styles.submitBtn}>Add Transaction</button>
+    </form>
+  );
+}
+export default ExpenseForm;
